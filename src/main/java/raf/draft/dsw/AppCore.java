2ecdc1a65446821dd8d8566003f609d1d8785757
filip <fcobanin@gmail.com>
2024-10-08 15:43:56 +0200

    First test push.

--- src/main/java/raf/draft/dsw/AppCore.java
+++ src/main/java/raf/draft/dsw/AppCore.java
@@ -4,6 +4,7 @@ import raf.draft.dsw.core.ApplicationFramework;
 
 public class AppCore {
     public static void main(String[] args) {
+        // test
         ApplicationFramework appCore = new ApplicationFramework();
     }
 }
