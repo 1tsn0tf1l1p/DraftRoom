fa1277bc53b8741cb1574eee43872e42bec59de5
filip <fcobanin@gmail.com>
2024-10-11 14:42:44 +0200

    Removed unnecessary comments Co-authored-by: name <sovljanski04@gmail.com>

--- src/main/java/raf/draft/dsw/controller/ExitAction.java
+++ src/main/java/raf/draft/dsw/controller/ExitAction.java
@@ -8,14 +8,12 @@ import java.net.URL;
 
 public class ExitAction extends AbstractAction{
     public ExitAction(){
-        //bitno
         putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F4, ActionEvent.ALT_MASK));
         putValue(SMALL_ICON, loadIcon("/images/exit.png"));
         putValue(NAME, "Exit");
         putValue(SHORT_DESCRIPTION, "Exit");
     }
 
-    //deo koda za ucitavanje ikonice...
     private Icon loadIcon(String path){
         Icon icon = null;
         URL ImageURL = getClass().getResource(path);
