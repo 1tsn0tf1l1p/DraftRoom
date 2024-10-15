e920e3b70bd3907e0131a44209875ecf01791e81
Dimitrije Sovljanski <sovljanski04@gmail.com>
2024-10-15 18:37:44 +0200

    Added features for actions (Week 3)

--- src/main/java/raf/draft/dsw/gui/swing/AboutUsFrame.java
+++ src/main/java/raf/draft/dsw/gui/swing/AboutUsFrame.java
@@ -11,22 +11,25 @@ public class AboutUsFrame extends JFrame {
 
     private void initialize(){
         Toolkit kit = Toolkit.getDefaultToolkit();
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
         Dimension screenSize = kit.getScreenSize();
         int screenHeight = screenSize.height;
         int screenWidth = screenSize.width;
+        setSize(screenWidth / 4, screenHeight / 4);
+
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("About us - DraftRoom");
-        this.setVisible(true);
+
         JTextPane student1 = new JTextPane();
         JTextPane student2 = new JTextPane();
-
         student1.setText("Filip Čobanin SI86/24");
         student2.setText("Dimitrije Šovljanski SI90/24");
+
         JPanel j1 = new JPanel();
-        j1.setLayout(new BorderLayout());
-        j1.add(student1, BorderLayout.NORTH);
-        j1.add(student2, BorderLayout.SOUTH);
+        j1.setLayout(new FlowLayout());
+        j1.add(student1);
+        j1.add(student2);
         this.getContentPane().add(j1, BorderLayout.CENTER);
     }
 
