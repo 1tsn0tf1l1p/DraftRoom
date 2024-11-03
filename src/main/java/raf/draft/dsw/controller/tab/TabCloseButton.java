ee4b97f9c1a8ecaa0cb46564b673c4baa4a7e756
filipcobanin <fcobanin@gmail.com>
2024-11-04 00:30:37 +0100

    Updated the TabCloseButton to a more modern look

--- src/main/java/raf/draft/dsw/controller/tab/TabCloseButton.java
+++ src/main/java/raf/draft/dsw/controller/tab/TabCloseButton.java
@@ -1,56 +1,48 @@
 package raf.draft.dsw.controller.tab;
 
 import javax.swing.*;
-import javax.swing.plaf.metal.MetalIconFactory;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class TabCloseButton extends JPanel {
 
     public TabCloseButton(final Component tab, String title) {
         setOpaque(false);
-        FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER, 3, 3);
-        setLayout(flowLayout);
+        setLayout(new FlowLayout(FlowLayout.CENTER, 3, 3));
+
         JLabel lbTitle = new JLabel(title);
         add(lbTitle);
-        JButton button = new JButton(MetalIconFactory.getInternalFrameCloseIcon(13));
-        button.setMargin(new Insets(0, 0, 0, 0));
-        button.addMouseListener((MouseListener) new CloseListener(tab));
-        add(button);
-    }
 
-    private static class CloseListener implements MouseListener {
-        private final Component tab;
+        JButton closeButton = new JButton("✕");
+        closeButton.setPreferredSize(new Dimension(16, 16));
+        closeButton.setBorder(BorderFactory.createEmptyBorder());
+        closeButton.setContentAreaFilled(false);
+        closeButton.setFocusPainted(false);
+        closeButton.setToolTipText("Close tab");
 
-        public CloseListener(Component tab) {
-            this.tab = tab;
-        }
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            if (e.getSource() instanceof JButton clickedButton) {
-                Container parent = clickedButton.getParent();
-                while (parent != null && !(parent instanceof JTabbedPane)) {
-                    parent = parent.getParent();
-                }
-                if (parent instanceof JTabbedPane tabbedPane) {
-                    tabbedPane.remove(tab);
-                }
+        closeButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                closeButton.setForeground(Color.RED);
             }
-        }
 
-        @Override
-        public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {
+                closeButton.setForeground(Color.BLACK);
+            }
+        });
 
-        @Override
-        public void mouseReleased(MouseEvent e) {}
+        closeButton.addActionListener(e -> {
+            Container parent = tab.getParent();
+            while (parent != null && !(parent instanceof JTabbedPane)) {
+                parent = parent.getParent();
+            }
+            if (parent instanceof JTabbedPane tabbedPane) {
+                tabbedPane.remove(tab);
+            }
+        });
 
-        @Override
-        public void mouseEntered(MouseEvent e) {}
-
-        @Override
-        public void mouseExited(MouseEvent e) {}
+        add(closeButton);
     }
-
 }
