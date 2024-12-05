b358de36e918ef8927c2697e399df007b88938b0
filipcobanin <fcobanin@gmail.com>
2024-12-05 11:10:28 +0100

    Adding elements to wrong coordinates while zoomed in/out fixed in AddState

--- src/main/java/raf/draft/dsw/controller/state/AddState.java
+++ src/main/java/raf/draft/dsw/controller/state/AddState.java
@@ -16,61 +16,75 @@ public class AddState implements RoomState {
     private Dimension originalSize;
     private RoomView roomView;
     private RoomElementFactory factory;
+
     public AddState(RoomView roomView) {
         this.roomView = roomView;
         this.originalSize = roomView.getOriginalSize();
         this.factory = roomView.getFactory();
     }
+
     @Override
     public void handleMouseClick(MouseEvent e) {
-        this.originalSize = new Dimension(800, 600);
-        RoomElement newBed = factory.create("bed", e);
-        CreateRoomFrame createRoomFrame = new CreateRoomFrame(newBed);
+        Point unscaledPoint = unscalePoint(e.getPoint());
+
+        MouseEvent unscaledEvent = new MouseEvent(
+                e.getComponent(),
+                e.getID(),
+                e.getWhen(),
+                e.getModifiersEx(),
+                (int) unscaledPoint.getX(),
+                (int) unscaledPoint.getY(),
+                e.getClickCount(),
+                e.isPopupTrigger(),
+                e.getButton()
+        );
+
+        RoomElement newElement = factory.create("bed", unscaledEvent);
+        CreateRoomFrame createRoomFrame = new CreateRoomFrame(newElement);
         createRoomFrame.setVisible(true);
 
         createRoomFrame.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosed(java.awt.event.WindowEvent e) {
-                Painter painter = factory.createPainter(newBed);
+                Painter painter = factory.createPainter(newElement);
                 roomView.getPainters().add(painter);
-                roomView.getRoom().addChild(newBed);
+                roomView.getRoom().addChild(newElement);
                 roomView.getRoomRectangle().repaint();
             }
         });
     }
 
+
     @Override
     public void handleMouseDrag(MouseEvent e) {
-
     }
 
     @Override
     public void handleMousePressed(MouseEvent e) {
-
     }
 
     @Override
     public void handleKeyPress(KeyEvent e) {
-
     }
 
     @Override
     public void handleMouseWheelMoved(MouseWheelEvent e) {
-
     }
 
     @Override
     public void handleMouseRelease(MouseEvent e) {
-
     }
 
     @Override
     public void enterState() {
-
     }
 
     @Override
     public void exitState() {
+    }
 
+    private Point unscalePoint(Point point) {
+        double zoomFactor = roomView.getZoomFactor();
+        return new Point((int) (point.x / zoomFactor), (int) (point.y / zoomFactor));
     }
 }
